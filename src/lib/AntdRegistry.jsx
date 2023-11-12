6a7f0c537807684f50cefbbcccf5bc9d42394bc8
Mukhlis Adhe Purwanto <adhemukhlis@gmail.com>
2023-11-12 19:15:58 +0000

    update import React to destructure

--- src/lib/AntdRegistry.jsx
+++ src/lib/AntdRegistry.jsx
@@ -2,11 +2,11 @@
 
 import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs'
 import { useServerInsertedHTML } from 'next/navigation'
-import React from 'react'
+import { useMemo, useRef } from 'react'
 
 const StyledComponentsRegistry = ({ children }) => {
-	const cache = React.useMemo(() => createCache(), [])
-	const isServerInserted = React.useRef(false)
+	const cache = useMemo(() => createCache(), [])
+	const isServerInserted = useRef(false)
 	useServerInsertedHTML(() => {
 		// avoid duplicate css insert
 		if (isServerInserted.current) {
